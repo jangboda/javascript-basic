b3d33505538c595af0c307602f2f7f123a759ce8
jangboda <106316984+jangboda@users.noreply.github.com>
2022-10-04 23:19:24 +0900

    javascript basic grammer
    if, if else number string etc

---
+++ operater.js
@@ -0,0 +1,13 @@
+alert(1==1)
+alert("one"="one")
+alert("one"="two")
+alert("one"="0")
+alert(null == undefined);       //true
+alert(null === undefined);      //false
+alert(true == 1);               //true
+alert(true === 1);              //false
+alert(true == '1');             //true
+alert(true === '1');            //false
+ 
+alert(0 === -0);                //true
+alert(NaN === NaN);
