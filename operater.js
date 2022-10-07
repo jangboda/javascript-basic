8754554e483c7c765d729357ad984e3124fd12b3
jangboda <106316984+jangboda@users.noreply.github.com>
2022-10-07 23:06:11 +0900

    javascript practicing
    funtion array

--- operater.js
+++ operater.js
@@ -1,3 +1,4 @@
+
 alert(1==1)
 alert("one"="one")
 alert("one"="two")
@@ -11,3 +12,24 @@ alert(true === '1');            //false
  
 alert(0 === -0);                //true
 alert(NaN === NaN);
+
+while(true){
+    document.write('coding everybody <br />');
+    
+  
+ for(var i = 0; i<10; i++){
+     for(var j = 0; j<10; j++){
+        document.write(coding everyday +i+j+ '<br />');
+     }
+ }   
+
+ function numbering(){
+    i = 0;
+    while(i < 10){
+    document.write(i);
+    i += 1;
+}   
+}
+numbering();
+
+var member =['eogoing','k8805','dadadas']
